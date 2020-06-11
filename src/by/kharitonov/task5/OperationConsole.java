53d333201b177953d31c96babdd67ff81eef2f4c
Alexey <senator220291@gmail.com>
2020-06-11 14:24:59 +0300

    First commit

---
+++ src/by/kharitonov/task5/OperationConsole.java
@@ -0,0 +1,11 @@
+package by.kharitonov.task5;
+
+public class OperationConsole {
+    public void printSumResult(int result) {
+        System.out.printf("%nSum of numbers = %d", result);
+    }
+
+    public void printProductResult(long result) {
+        System.out.print("\nProduct of numbers = " + result);
+    }
+}
